e386b109757b461a034a074ddb3e43d1c58f9c27
ymtoo <ymtoo86@gmail.com>
2023-06-09 11:00:02 +0800

    fix: prob of applying the transforms

--- src/augment.jl
+++ src/augment.jl
@@ -8,13 +8,21 @@ export rand_tanhdistortion
 $(TYPEDSIGNATURES)
 
 Random time sample shift of spectrograms `x`.
+
+# Arguments
+- x: multi-channel signals
+- p: the probability of applying this transform
 """
-function rand_timesampleshift(x::AbstractArray)
+function rand_timesampleshift(x::AbstractArray; p = 0.5)
     map(eachslice(x; dims = tuple(2:ndims(x)...))) do x1
-        xn = size(x1, 1)
-        max_shift = xn ÷ 2
-        shiftsample = rand(-max_shift:max_shift)
-        circshift(x1, (shiftsample,))
+        if rand() > p
+            xn = size(x1, 1)
+            max_shift = xn ÷ 2
+            shiftsample = rand(-max_shift:max_shift)
+            circshift(x1, (shiftsample,))::typeof(x1)
+        else
+            x1
+        end
     end |> stack
 end
 
@@ -22,10 +30,14 @@ end
 $(TYPEDSIGNATURES)
 
 Random flip `x` upside-down.
+
+# Arguments
+- x: multi-channel signals
+- p: the probability of applying this transform
 """
-function rand_polarityinversion(x::AbstractArray{T}) where {T}
+function rand_polarityinversion(x::AbstractArray{T}; p = 0.5) where {T}
     map(eachslice(x; dims = tuple(2:ndims(x)...))) do x1
-        a = rand() ≥ 0.5 ? one(T) : -one(T)
+        a = rand() ≥ p ? one(T) : -one(T)
         x1 .* a
     end |> stack
 end
@@ -35,15 +47,24 @@ $(TYPEDSIGNATURES)
 
 Random tanh distortion of `x`.
 
+# Arguments
+- x: multi-channel signals
+- min_distortion: minimum "amount" of distortion to apply to the signal
+- max_distortion: maximum "amount" of distortion to apply to the signal
+- p: the probability of applying this transform
+
 Reference
 https://github.com/iver56/audiomentations/blob/main/audiomentations/augmentations/tanh_distortion.py
 """
-function rand_tanhdistortion(x::AbstractArray{T}, min_distortion = T(0.01), max_distortion = T(0.7)) where {T}
+function rand_tanhdistortion(x::AbstractArray{T}, 
+                             min_distortion = T(0.01), 
+                             max_distortion = T(0.7);
+                             p = 0.5) where {T}
     map(eachslice(x; dims = tuple(2:ndims(x)...))) do x1
-        if rand() > 0.5
+        if rand() > p
             distortion_amount = (max_distortion - min_distortion) * rand(T) + min_distortion
-            p = 1 - 0.99 * distortion_amount
-            threshold = quantile(abs.(x1), p) |> T
+            γ = 1 - 0.99 * distortion_amount
+            threshold = quantile(abs.(x1), γ) |> T
             gain_factor = T(0.5) / (threshold + T(1e-6))
             dist_x1 = tanh.(gain_factor .* x1)
             rms_x1 = sqrt(mean(abs2, x1))
@@ -52,7 +73,7 @@ function rand_tanhdistortion(x::AbstractArray{T}, min_distortion = T(0.01), max_
                 post_gain = rms_x1 / rms_dist_x1
                 dist_x1 .*= post_gain
             end
-            dist_x1
+            dist_x1::typeof(x1)
         else
             x1
         end
