4e5639653880d3727c1b72a2546bc77aef24eb36
ymtoo <ymtoo86@gmail.com>
2023-07-20 15:37:35 +0800

    perf: weakly dependent on TimeScaleModification

---
+++ ext/DataUtilsTimeScaleModificationExt.jl
@@ -0,0 +1,44 @@
+module DataUtilsTimeScaleModificationExt
+
+using DataUtils
+using TimeScaleModification: pitchshift, timestretch, WSOLA, hanning
+
+"""
+Random pitch shifting of `x`.
+
+# Arguments
+- x: time-series signal
+- max_semitones: maximum semitones to shift
+- n: window size
+- synhopsize: hop size of the synthesis window
+- p: the probability of applying this transform
+"""
+function DataUtils.rand_pitchshift(x::AbstractVector{T}, max_semitones::Real = 12, n::Int = 128, synhopsize::Int = 64; p = 0.5) where {T}
+    if rand() ≤ p
+        semitones = rand(-max_semitones..max_semitones)
+        pitchshift(WSOLA(n, synhopsize, hanning, 10), x, semitones)
+    else
+        collect(x) # for type stability
+    end
+end
+
+"""
+Random time stretching of `x`.
+
+# Arguments
+- x: time-series signal
+- max_shiftedspeed: maximum shited speed
+- n: window size
+- synhopsize: hop size of the synthesis window
+- p: the probability of applying this transform
+"""
+function DataUtils.rand_timestretch(x::AbstractVector{T}, max_shiftedspeed::Real = 0.1, n::Int = 128, synhopsize::Int = 64; p = 0.5) where {T}
+    if rand() ≤ p
+        speed = rand((1-max_shiftedspeed)..(1+max_shiftedspeed))
+        timestretch(WSOLA(n, synhopsize, hanning, 10), x, speed)
+    else
+        collect(x) # for type stability
+    end
+end
+
+end
--- src/augment.jl
+++ src/augment.jl
@@ -1,6 +1,5 @@
 using IntervalSets
 using Statistics
-using TimeScaleModification: pitchshift, timestretch, WSOLA, hanning
 
 export rand_timesampleshift
 export rand_polarityinversion
@@ -106,43 +105,8 @@ function rand_addgaussiansnr(x::AbstractVector{T},
     end
 end
 
-"""
-Random pitch shifting of `x`.
-
-# Arguments
-- x: time-series signal
-- max_semitones: maximum semitones to shift
-- n: window size
-- synhopsize: hop size of the synthesis window
-- p: the probability of applying this transform
-"""
-function rand_pitchshift(x::AbstractVector{T}, max_semitones::Real = 12, n::Int = 128, synhopsize::Int = 64; p = 0.5) where {T}
-    if rand() ≤ p
-        semitones = rand(-max_semitones..max_semitones)
-        pitchshift(WSOLA(n, synhopsize, hanning, 10), x, semitones)
-    else
-        collect(x) # for type stability
-    end
-end
-
-"""
-Random time stretching of `x`.
-
-# Arguments
-- x: time-series signal
-- max_shiftedspeed: maximum shited speed
-- n: window size
-- synhopsize: hop size of the synthesis window
-- p: the probability of applying this transform
-"""
-function rand_timestretch(x::AbstractVector{T}, max_shiftedspeed::Real = 0.1, n::Int = 128, synhopsize::Int = 64; p = 0.5) where {T}
-    if rand() ≤ p
-        speed = rand((1-max_shiftedspeed)..(1+max_shiftedspeed))
-        timestretch(WSOLA(n, synhopsize, hanning, 10), x, speed)
-    else
-        collect(x) # for type stability
-    end
-end
+function rand_pitchshift end
+function rand_timestretch end
 
 """
 Apply augmentation `f` on each of the first dimension of `x`. 
