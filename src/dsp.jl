a25ade2c78bd764ea45bbfc9669928f044f1ffcb
ymtoo <ymtoo86@gmail.com>
2023-10-05 14:55:55 +0800

    feat: filter_resample takes in arrays

--- src/dsp.jl
+++ src/dsp.jl
@@ -1,11 +1,12 @@
 using SignalAnalysis 
+using SignalAnalysis: SampledSignal
 
 """
 $(SIGNATURES)
 
 Lowpass-filter, downsample if `f2` is not `nothing`, followed by highpass-filter `s`.
 """
-function filter_resample(s::AbstractArray{T}, f1, f2=nothing) where {T<:Real}
+function filter_resample(s::SampledSignal{T}, f1, f2=nothing) where {T<:Real}
     s = s .- mean(s; dims = 1)
     if !isnothing(f2)
         lpf = fir(127, zero(f1), f2; fs = framerate(s))
@@ -14,4 +15,8 @@ function filter_resample(s::AbstractArray{T}, f1, f2=nothing) where {T<:Real}
     end
     hpf = fir(127, f1; fs = framerate(s))
     T.(sfiltfilt(hpf, s))
+end
+
+function filter_resample(s::AbstractArray{T}, f1, f2=nothing; fs = 1) where {T<:Real}
+    filter_resample(signal(s, fs), f1, f2) |> samples
 end
